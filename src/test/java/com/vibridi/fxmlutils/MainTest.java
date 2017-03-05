2a778b6368d8694c1ff093bf2e7fdc5bb44396aa
gvaccari <tianxiang.vaccari@gmail.com>
2017-03-05 21:11:31 +0100

    First commit

---
+++ src/test/java/com/vibridi/fxmlutils/MainTest.java
@@ -0,0 +1,12 @@
+package com.vibridi.fxmlutils;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+    public void testApp() {
+        // TODO auto-generated code
+    }
+}
